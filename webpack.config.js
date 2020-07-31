3c65a9fb408b00e7f2eade2db555d87ec0b29130
MaccKOT <51321216+MaccKOT@users.noreply.github.com>
2020-07-31 12:50:51 +0400

    Webpack-merge for code splitting

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,9 @@
 const path = require('path');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { merge } = require('webpack-merge');
 
-module.exports = {
+const commonConfig = {
   entry: './src/index.js',
   output: {
     filename: 'main.js',
@@ -16,4 +17,25 @@ module.exports = {
       template: path.resolve(__dirname, 'src', 'index.html'),
     }),
   ],
+};
+
+const productionConfig = { mode: 'production', devtool: 'source-map' };
+
+const developmentConfig = {
+  mode: 'development',
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist',
+  },
+};
+
+module.exports = (env) => {
+  switch (env) {
+    case 'development':
+      return merge(commonConfig, developmentConfig);
+    case 'production':
+      return merge(commonConfig, productionConfig);
+    default:
+      throw new Error('No matching configuration was found!');
+  }
 };
