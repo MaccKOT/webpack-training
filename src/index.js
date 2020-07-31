c109d8830c45a64522db4d6e91c0404b52e4f407
MaccKOT <51321216+MaccKOT@users.noreply.github.com>
2020-07-31 13:15:41 +0400

    Babel module added

--- src/index.js
+++ src/index.js
@@ -3,3 +3,11 @@ import './index.css';
 const p = document.createElement('p');
 p.textContent = 'Hello from webpack!';
 document.body.append(p);
+
+//some modern ES6 code here
+const p2 = document.createElement('p');
+const numbers1 = [1, 2, 3, 4, 5, 6];
+const numbers2 = [7, 8, 9, 10];
+const numbers3 = [...numbers1, ...numbers2, ' ...Babel is working well!'];
+p2.textContent = numbers3.join(' ');
+document.body.appendChild(p2);
--- webpack.config.js
+++ webpack.config.js
@@ -23,6 +23,13 @@ const commonConfig = {
         test: /\.css$/,
         use: ['style-loader', 'css-loader'],
       },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /[\\/]node_modules[\\/]/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
     ],
   },
 };
